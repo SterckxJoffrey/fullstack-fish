73e99afa9c3ced821c2b8c29143cf8f47dabf4f6
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-31 16:17:59 +0200

    plus

--- 00-rest-api/drizzle.config.ts
+++ 00-rest-api/drizzle.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   dialect: 'sqlite',
   dbCredentials: {
     // Use the volume Railway en prod, local file for dev
-    url: process.env.NODE_ENV === 'production' ? '/data/sqlite.db' : 'sqlite.db',
+    url: 'sqlite.db',
   },
 });
