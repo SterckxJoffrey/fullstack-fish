b41afb3c1931ecd73999dd9ea01f92ed6a82395f
Joffrey Sterckx <147731791+SterckxJoffrey@users.noreply.github.com>
2026-03-31 16:13:43 +0200

    no instance db

--- 00-rest-api/src/core/db.ts
+++ 00-rest-api/src/core/db.ts
@@ -5,7 +5,7 @@ import * as fishspots from '../resources/fishspots/fishspots.schema';
 
 const isTest = process.env.NODE_ENV === 'test';
 const isProduction = process.env.NODE_ENV === 'production';
-const DB_PATH = isTest ? ':memory:' : (isProduction ? '/data/sqlite.db' : 'sqlite.db');
+const DB_PATH = isTest ? ':memory:' : (isProduction ?  'sqlite.db' : '');
 const sqlite = new Database(DB_PATH);
 
 export const db = drizzle({
